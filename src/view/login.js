b42546c60ef1e666ea16e1cdfa20192947bc7cef
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2023-02-21 16:26:20 -0800

    login page progress

--- src/view/login.js
+++ src/view/login.js
@@ -1,3 +1,34 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 export default function Login() {
-    return <h1>Login</h1>
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Vendia Care
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Vendia Care, where your health matters!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 }
