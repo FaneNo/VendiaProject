98a972a15bd947ecc6938066daae7b6d1f6c0534
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2023-02-21 16:34:05 -0800

    login progress

--- src/view/login.js
+++ src/view/login.js
@@ -1,34 +1,48 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import Form from "./Form";
+const { default: Box } = require("@mui/material/Box");
+const { default: Typography } = require("@mui/material/Typography");
+const { useTheme, useMediaQuery } = require("@mui/material");
+const Form = require("./Form");
 
-export default function Login() {
+function Login() {
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-    <Box>
-      <Box
-        width="100%"
-        backgroundColor={theme.palette.background.alt}
-        p="1rem 6%"
-        textAlign="center"
-      >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Vendia Care
-        </Typography>
-      </Box>
+    Box(
+      null,
+      Box({
+        width: "100%",
+        backgroundColor: theme.palette.background.alt,
+        p: "1rem 6%",
+        textAlign: "center"
+      },
+        Typography({
+          fontWeight: "bold",
+          fontSize: "32px",
+          color: "primary"
+        },
+          "Vendia Care"
+        )
+      ),
 
-      <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        p="2rem"
-        m="2rem auto"
-        borderRadius="1.5rem"
-        backgroundColor={theme.palette.background.alt}
-      >
-        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Vendia Care, where your health matters!
-        </Typography>
-        <Form />
-      </Box>
-    </Box>
+      Box({
+        width: isNonMobileScreens ? "50%" : "93%",
+        p: "2rem",
+        m: "2rem auto",
+        borderRadius: "1.5rem",
+        backgroundColor: theme.palette.background.alt
+      },
+        Typography({
+          fontWeight: "500",
+          variant: "h5",
+          sx: { mb: "1.5rem" }
+        },
+          "Welcome to Vendia Care, where your health matters!"
+        ),
+        Form(null)
+      )
+    )
   );
 }
+
+module.exports = Login;
+
