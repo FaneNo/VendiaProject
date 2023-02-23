7fa158f35175e6016aa0d8c90a8836b519a2538e
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2023-02-23 10:51:41 -0800

    gray background for home page

--- src/view/home.js
+++ src/view/home.js
@@ -1,30 +1,34 @@
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-    return <nav className="nav">
+  return (
+    <div className="background" style={{ backgroundColor: '#f0f0f0' }}>
+      <nav className="nav">
         <div className="homeImg" >
-            <Link to="/"><img className="vendiaLogo" src={require("../vendiaLogo.png")} /></Link>
+          <Link to="/"><img className="vendiaLogo" src={require("../vendiaLogo.png")} /></Link>
         </div>
         <div className="titleName" ><Link to="/" className="title">Vendia Care</Link></div>
         <ul>
-            <li>
-                <Link to="/fda">FDA</Link>   
-            </li>
-            <li>
-                <Link to="/patient">Patient</Link>
-            </li>
-            <li>
-                <Link to="/doctor">Doctor</Link>
-            </li>
-            <li>
-                <Link to="/new">New patient</Link>
-            </li>
-            <li>
-                <Link to="/register">Register</Link>
-            </li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-            
+          <li>
+            <Link to="/fda">FDA</Link>   
+          </li>
+          <li>
+            <Link to="/patient">Patient</Link>
+          </li>
+          <li>
+            <Link to="/doctor">Doctor</Link>
+          </li>
+          <li>
+            <Link to="/new">New patient</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
-    </nav>
+      </nav>
+    </div>
+  );
 }
