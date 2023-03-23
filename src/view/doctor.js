8cc8a9e4dee9cf7fe4659db944fecfb476e79cb4
FaneNo <102693139+FaneNo@users.noreply.github.com>
2023-03-22 22:36:53 -0700

    listing array working

--- src/view/doctor.js
+++ src/view/doctor.js
@@ -21,9 +21,6 @@ export default function Doctor() {
         listPatients();
     }
 
-    const redirect = async (patientID) => {
-        
-    }
     const navigate = useNavigate();
 
     useEffect(() => {
