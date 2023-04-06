274fab570ef7d6fe7c39577bccdf3a6677a34618
FaneNo <102693139+FaneNo@users.noreply.github.com>
2023-04-05 23:59:15 -0700

    fix patient page

--- src/view/patient.js
+++ src/view/patient.js
@@ -1,4 +1,4 @@
-import { json, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "../view/nav";
 import { useState, useEffect } from "react";
 import useJaneHopkins from "../hooks/useJaneHopkins";
@@ -39,52 +39,54 @@ export default function Patient() {
             </ul>
           </span>
         </div>
-            <div className="box" id="top-right-1">
-                <span className='list'>
-                    <ul>
-                        <li>Blood Type{patient?.bloodType}</li>
-                    </ul>
-                </span> 
-            </div>
-            <div className="box" id="top-right-2">
-                <span className='list'>
-                    <ul>
-                        <li>Temperature{patient?.temperature}</li>
-                    </ul>
-                </span>
-            </div>
-            <div className="box" id="top-right-3">
-                <span className='list'>
-                    <ul>
-                        <li>Blood Pressure{patient?.bloodPressure}</li>
-                    </ul>
-                </span>
-            </div>
-            <div className="box" id="bottom-left">
-                <span className='list'>
-                    <ul>
-                        <li>Height: {patient?.height}</li>
-                        <li>Weight:{patient?.weight}</li>
-                        <li>UUID:{patient?.uuid}</li>
-                        <li>Insurance Number:{patient?.insuranceNumber}</li>
-                        <li>ICD Health code:</li>
-                        <li>Oxygen Saturation:{patient?.oxygenSaturation}</li>
-                        <li>Currently Insured:{patient?.currentlyInsured}</li>
-                    </ul>
-                </span>
-            </div>
-            <div className="box" id="bottom-right">
-                <span className='list' id=''>
-                    <ul>
-                        <li>Family History:{patient?.familyHistory}</li>
-                        <li>Address:{patient?.address}</li>
-                        <li>List of visit:{patient?.visits}</li>
-                        <li>Allergies:</li>
-                        <li>Current Medication:</li>
-                    </ul>
-                </span>
-            </div>
+        <div className="box" id="top-right-1">
+          <span className="list">
+            <ul>
+              <li>Blood Type: {patient?.bloodType}</li>
+            </ul>
+          </span>
         </div>
-    </> 
-    )
+        <div className="box" id="top-right-2">
+          <span className="list">
+            <ul>
+              <li>Temperature: {patient?.temperature}</li>
+            </ul>
+          </span>
+        </div>
+        <div className="box" id="top-right-3">
+          <span className="list">
+            <ul>
+              <li>Blood Pressure: {patient?.bloodPressure}</li>
+            </ul>
+          </span>
+        </div>
+        <div className="box" id="bottom-left">
+          <span className="list">
+            <ul>
+              <li>Height: {patient?.height}</li>
+              <li>Weight:{patient?.weight}</li>
+              <li>UUID:{patient?.uuid}</li>
+              <li>Insurance Number:{patient?.insuranceNumber}</li>
+              <li>ICD Health code: {patient?.icdHealthCodes[0].code}</li>
+              <li>Oxygen Saturation:{patient?.oxygenSaturation}</li>
+              <li>Currently Insured:{patient?.currentlyInsured}</li>
+            </ul>
+          </span>
+        </div>
+        <div className="box" id="bottom-right">
+          <span className="list" id="">
+            <ul>
+              <li>Family History:{patient?.familyHistory}</li>
+              <li>Address:{patient?.address}</li>
+              <li>List of visit:</li>
+              <li>Allergies: {patient?.allergies[0].allergy}</li>
+              <li>
+                Current Medication: {patient?.currentMedications[0].medication}
+              </li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
