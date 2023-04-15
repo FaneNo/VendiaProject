944f2c11c8922b31dd53330f47e268a7ed246d19
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2023-04-15 03:04:33 -0700

    sorta changed logout button to look better

--- src/view/logout.js
+++ src/view/logout.js
@@ -21,13 +21,37 @@ const Signout = () => {
 
   return (
     <>
-      <nav>
-        <div>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      </nav>
+      <style>
+        {`
+          .logout-container {
+            display: flex;
+            justify-content: flex-end;
+          }
+
+          .logout-button {
+            background-color: #4c90af;
+            color: white;
+          //   border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background-color 0.3s ease-in-out;
+          }
+
+          .logout-button:hover {
+            background-color: #3e51aa;
+          }
+        `}
+      </style>
+      <div className="logout-container">
+        <button className="logout-button" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </>
   );
 };
 
 export default Signout;
+
+
